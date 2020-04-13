341bdd245bb3c31acb5a501201bc61eb63c10f60
thsvr <vr.ths.zd@gmail.com>
2020-04-13 19:01:55 -0300

    Add complete app created by react tutorial - Rocketseat.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Routes from "./routes";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import "./styles.css";
+
+import Header from './components/Header';
+import Main from './pages/main';
+
+
+const App = () => (
+  <div className="App">
+    <Header />
+    <Routes />
+  </div>
+);
+
+
+// function App() {
+//   class App extends Component {
+//     render() {
+//     return (
+//       <div className="App">
+//         <h1>Hello Rocketseat</h1>
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
---
+++ src/components/Header/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import "./styles.css";
+
+const Header = () => (
+  <header id= "main-header">JSHunt</header>
+);
+
+export default Header;
+
+// other way to write the above const
+// class Header extends Component {
+//   render () {
+//     return <h1>Hello</h1>
+//   }
+// }
